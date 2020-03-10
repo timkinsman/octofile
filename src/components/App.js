3211051e64ce78171e7f7c58bd302ce3450726e9
timkinsman <tkinsm@gmail.com>
2020-03-10 21:08:32 +1030

    octohub login screen

--- src/components/App.js
+++ src/components/App.js
@@ -5,9 +5,20 @@ const App = () => {
     var client = github.client();
 
     var ghuser = client.user('timkinsman');
-    console.log(ghuser)
+    console.log(ghuser.infoAsync())
 
-    return <div>App</div>
+    return (
+        <div className="ui middle aligned center aligned grid">
+            <div className="column">
+                <img src={require('../assets/images/github.svg')} className="ui centered small image" />
+                <h1 className="ui grey inverted header">Hi!</h1>
+                <div className="ui inverted transparent icon massive input">
+                    <input type="text" placeholder="GitHub username..." />
+                    <i className="search icon"></i>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default App;
