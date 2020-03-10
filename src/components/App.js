1ff0e8b925e486fffa669d56e66517e14eb0d4d4
timkinsman <tkinsm@gmail.com>
2020-03-10 23:16:13 +1030

    OctoDisplay octonode

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,6 @@ import React from 'react';
 import OctoSearch from './OctoSearch';
 
 const App = () => {
-    var github = require('octonode');
-    var client = github.client();
-
-    var ghuser = client.user('timkinsman');
-    console.log(ghuser.infoAsync())
-
     return (
         <OctoSearch />
     )
--- src/components/OctoDisplay.js
+++ src/components/OctoDisplay.js
@@ -1,6 +1,12 @@
 import React from 'react';
 
 const OctoDisplay = () => {
+    var github = require('octonode');
+    var client = github.client();
+
+    var ghuser = client.user('timkinsman');
+    console.log(ghuser)
+
     return (
         <div>OctoDisplay</div>
     )
