a1396633eaa5645f9cfa28cf2f62805ba58e2c11
timkinsman <tkinsm@gmail.com>
2020-03-10 22:02:15 +1030

    refactor into OctoSearch

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import OctoSearch from './OctoSearch';
 
 const App = () => {
     var github = require('octonode');
@@ -8,16 +9,7 @@ const App = () => {
     console.log(ghuser.infoAsync())
 
     return (
-        <div className="ui middle aligned center aligned grid">
-            <div className="column">
-                <img src={require('../assets/images/github.svg')} className="ui centered small image" />
-                <h1 className="ui grey inverted header">Hi!</h1>
-                <div className="ui inverted transparent icon massive input">
-                    <input type="text" placeholder="GitHub username..." />
-                    <i className="search icon"></i>
-                </div>
-            </div>
-        </div>
+        <OctoSearch />
     )
 }
 
---
+++ src/components/OctoSearch.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const OctoSearch = () => {
+    return (
+        <div className="ui middle aligned center aligned grid">
+            <div className="column">
+                <img src={require('../assets/images/github.svg')} className="ui centered small image" />
+                <h1 className="ui grey inverted header">Hi!</h1>
+                <div className="ui inverted transparent icon massive input">
+                    <input type="text" placeholder="GitHub username..." />
+                    <i className="search icon"></i>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default OctoSearch;
